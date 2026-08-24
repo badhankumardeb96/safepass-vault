8366daab3b34ce9d042911578b7223c0321645eb
badhankumardeb96 <badhandeb96@gmail.com>
2026-08-24 15:35:29 +0600

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-// Supabase Global Client Initialization
-const supabaseUrl = 'https://vgjsoicsmmzahhsuworg.supabase.co';
-const supabaseKey = 'sb_publishable_NkMibVnz7Vt6CAHuSTaQZw_zpUFGNsv';
-
-// উইন্ডো অবজেক্টে supabase সেট করা যাতে অন্য যেকোনো ফাইল থেকে সরাসরি ব্যবহার করা যায়
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
